76ad49d53a9485a83a4df24c5f6c227cc77d1b0e
Denmark Garcia <denmark.garcia@Denmarks-MacBook-Pro.local>
2024-03-03 16:55:29 +0800

    tetest

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
+	@GetMapping("/message")
 	public String message(){
 		return "welcome to javatechie";
 	}
